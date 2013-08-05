fd91081862534cdd1efc71771341c53e7fa75f31
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2013-08-05 15:23:17 -0700

    Finish Kata

--- kata.js
+++ kata.js
@@ -1,13 +1,16 @@
-(function($) {
-  console.log("Hello");
-  
+(function($) {  
   $("#submit").click(function() {
-  
-    $(".item").foreach(function() {
-      $(this).find(
+    var display = $(".display");
+    var total = "0";
+    display.empty();
+    $(".item").each(function() {
+      var name = $(this).find(".name").text();
+      var price = $(this).find(".price").text();
+      var quantity = $(this).find(".quantity").val();
+      var p = $("<p>");
+      p.text("Name: " + name + " Quantity: " + quantity + " Total Price: " + price);
+      display.append(p);
     });
+    display.append("<p>Total Value: " + total + "</p>");
   });
-  
-  
-  
 })(jQuery);
